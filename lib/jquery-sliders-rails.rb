58e31d5fb782b3ee2653440de6fb96cb05df66c5
Andrew De Ponte <cyphactor@gmail.com>
2012-04-24 01:14:08 -0700

    Initial commit.

---
+++ lib/jquery-sliders-rails.rb
@@ -0,0 +1,9 @@
+require "jquery-sliders-rails/version"
+
+module Jquery
+  module Sliders
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery-sliders-rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Sliders
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
